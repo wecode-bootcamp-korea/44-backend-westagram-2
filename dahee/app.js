2f46f24fd91a22683ddc5c1870e13e8dc5868374
dahee jang <wol04255@gmail.com>
2023-03-22 17:07:03 +0900

    [dahee_Update After Review] Assignment 1: Express API - 초기 환경 세팅

--- dahee/app.js
+++ dahee/app.js
@@ -1,31 +1,35 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const dotenv = require('dotenv')
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { DataSource } = require('typeorm');
 
-dotenv.config()
+require('dotenv').config();
 
-app.use(cors())
-app.use(express.json())
-//app.use(morgan("combined"))
+app.use(cors());
+app.use(express.json());
+app.use(morgan('combined'));
 
-const { DataSource } = require('typeorm')
+const appDataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
-const myDataSource = new DataSource({
-    type: process.env.DB_CONNECTION,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-})
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+    appDataSource.destroy();
+  });
 
-myDataSource.initialize()
-    .then(() => {
-        console.log("Data Source has been initialized!")
-    })
+const PORT = process.env.PORT;
 
-app.listen(3000, () => {
-    console.log('port is listening')
-})
-
+app.listen(PORT, () => {
+  console.log('Server is listening on port ${PORT}');
+});
