d05332283a8cdbeef50289522a70c00ef24b0e52
dahee jang <wol04255@gmail.com>
2023-03-23 14:24:25 +0900

    [장다희] Assignment 1: Express API - 초기 환경 세팅 (gitignore & 수정)

--- dahee/app.js
+++ dahee/app.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const app = express();
 const morgan = require('morgan');
-
 const { DataSource } = require('typeorm');
+
+const app = express();
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
@@ -20,16 +21,18 @@ const appDataSource = new DataSource({
   database: process.env.DB_DATABASE,
 });
 
-//health check
+appDataSource
+  .initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization', err);
+  });
+
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
-
-appDataSource.initialize().then(() => {
-  console.log('Data Source has been initialized!');
-});
-
-const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
