08e75a499bb274011c3fed048515ffd3ced69004
dahee jang <wol04255@gmail.com>
2023-03-25 15:15:18 +0900

    [Merge] study comments added

--- dahee/app.js
+++ dahee/app.js
@@ -1,3 +1,5 @@
+//code 짜기 전에 sql문으로 db구축 먼저 하는 게 순서에 맞음
+
 require('dotenv').config();
 
 const express = require('express');
@@ -36,6 +38,12 @@ app.get('/ping', (req, res) => {
 
 app.post('/users', async (req, res) => {
   const { name, email, password, profileImage } = req.body;
+
+  //필요한 데이터가 다 오지 않은 경우에 대한 error handling
+  //try catch 써야 하는 경우가 있고, if로 하는 경우가 있는데, 내가 예측 가능한 에러면 if가 나음
+  if (!email || !password || !name)
+    return res.status(400).json({ message: 'KEY_ERROR' });
+
   await appDataSource.query(
     `INSERT INTO users (
       name,
@@ -50,6 +58,9 @@ app.post('/users', async (req, res) => {
 
 app.post('/posts', async (req, res) => {
   const { title, content, userId } = req.body;
+
+  //에러 핸들링 추가 - status code 잘 쓰는 것도 중요
+
   const insert = await appDataSource.query(
     `INSERT INTO posts (
       title,
@@ -75,6 +86,17 @@ app.get('/posts', async (req, res) => {
   res.status(200).json({ data });
 });
 
+//***함수의 결과의 첫번째 인덱스를 const [post]에 담을 때, 배열이면 배열에서 풀리고 객체로 감
+//let a = [1,2,3] const [x,y] =a console.log(x) > 1, console.log(y) > 2
+//?는 mysql2가 지원하는 기능임, ${}보다 ? [] 페어를 더 권장함 ({} 객체로 만들어서 쓰기도 함)
+
+//Asgmt5 - end point '/users/posts?userId=1' 쿼리 파라미터, GET 메서드의 body 역할
+//query parameter는 프론트가 맘대로 넣을 수 있음, 모두 다 string으로 옴
+//table 별칭은 이름 initial로 많이 지음
+//Group By 생략될 때도 있음
+//subquery는 안 쓸 수 있으면 안 쓰는 게 좋음, 한 번 더 query 호출해야 하니까
+//APPLICATION JOIN은 객체 키의 value로 정의, 객체.key = value(다른 table)
+
 app.get('/users/:userId', async (req, res) => {
   const { userId } = req.params;
   const data = await appDataSource.query(
@@ -99,52 +121,6 @@ app.get('/users/:userId', async (req, res) => {
     WHERE u.id = ${userId};`
   );
   res.status(200).json({ data });
-});
-
-app.patch('/posts/:userId/:postId/', async (req, res) => {
-  const { userId, postId } = req.params;
-  const { title, content } = req.body;
-  await appDataSource.query(
-    `UPDATE posts   
-    SET
-      title = ?,
-      content = ? 
-    WHERE user_id = ${userId} AND id = ${postId};`,
-    [title, content]
-  );
-
-  const updatedPost = await appDataSource.query(
-    `SELECT 
-        p.user_id userId,
-        u.name userName,
-        p.id postingId,
-        p.title postingTitle,
-        p.content postingContent
-      FROM posts p
-      JOIN users u ON p.user_id = u.id
-      WHERE p.user_id = ${userId} AND p.id = ${postId}`
-  );
-  res.status(200).json({ updatedPost });
-});
-
-app.delete('/posts/:postId', async (req, res) => {
-  const { postId } = req.params;
-  await appDataSource.query(
-    `DELETE FROM posts
-    WHERE posts.id = ${postId}`
-  );
-  res.status(204).json({ message: 'postingDeleted' });
-});
-
-app.post('/likes/:userId/:postId', async (req, res) => {
-  const { userId, postId } = req.params;
-  const like = await appDataSource.query(
-    `INSERT INTO likes (
-      user_id, 
-      post_id
-    ) VALUES ( ${userId}, ${postId} )`
-  );
-  res.status(201).json({ message: 'likeCreated' });
 });
 
 app.listen(PORT, () => {
