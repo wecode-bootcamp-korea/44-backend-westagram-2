ce8ab1d11e948212cae4ccfe93ec5b18b3f05821
dahee jang <wol04255@gmail.com>
2023-03-28 11:40:34 +0900

    [FIX] 주석삭제

--- dahee/app.js
+++ dahee/app.js
@@ -17,21 +17,6 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
 });
 
-/*app.get('/posts', async (req, res) => {
-  const data = await appDataSource.query(
-    `SELECT
-      u.id userId,
-      u.profile_image userProfileImage,
-      p.id postingId,
-      p.image_url postingImageUrl,
-      p.content postingContent
-    FROM posts p
-    JOIN users u ON p.user_id = u.id`
-  );
-  res.status(200).json({ data });
-});
-*/
-
 const start = async () => {
   try {
     app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
