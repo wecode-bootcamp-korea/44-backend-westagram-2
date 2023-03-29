25f1abe872d18615c072abb201f262a83f185094
SH <robot4097@gmail.com>
2023-03-29 10:35:56 +0900

    [feat] 정규표현식 비밀번호 유효성 검사 userService에 추가

--- Sanghyun/controllers/userController.js
+++ Sanghyun/controllers/userController.js
@@ -3,6 +3,16 @@ const userService = require("../services/userService");
 const createUser = async (req, res) => {
   try {
     const { userName, email, profileImage, userPassword } = req.body;
+
+    const pwValidation = new RegExp(
+      "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})"
+    );
+    if (!pwValidation.test(userPassword)) {
+      const err = new Error("password is not valid");
+      err.statusCode = 400;
+      throw err;
+    }
+
     if (!userName || !email || !profileImage || !userPassword) {
       return res.status(400).json({ message: "Missing required fields" });
     }
