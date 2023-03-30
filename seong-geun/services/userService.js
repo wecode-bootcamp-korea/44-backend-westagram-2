649aecf0f9ac5f0c477d0cc1fde5938c6bffd9d3
Lim seong-geun <123074891+lsg622@users.noreply.github.com>
2023-03-30 13:34:05 +0900

    WIP

--- seong-geun/services/userService.js
+++ seong-geun/services/userService.js
@@ -1,4 +1,5 @@
 const userDao = require("../models/userDao");
+const bcrypt = require("bcrypt");
 
 const signUp = async (id, userProfileImage, userName, age, email, password) => {
   // password validation using REGEX
@@ -6,6 +7,11 @@ const signUp = async (id, userProfileImage, userName, age, email, password) => {
     "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})"
   );
 
+  //Bcrypt 비밀번호 암호화
+  const saltRounds = 12; // 기본이 10번, 숫자가 올라갈수록 연산 시간과 보안이 높아진다.
+  const makeHash = await bcrypt.hash(password, saltRounds);
+
+  const validPassword = await bcrypt.compare(req.body.passwrod, users.password);
   if (!pwValidation.test(password)) {
     const err = new Error("PASSWORD_IS_NOT_VALID");
     err.statusCode = 409;
@@ -23,6 +29,9 @@ const signUp = async (id, userProfileImage, userName, age, email, password) => {
 
   return createUser;
 };
+
+//Bcrypt 검증
+const checkHash = await checkHash("passwrod", hashedPassword);
 
 module.exports = {
   signUp,
